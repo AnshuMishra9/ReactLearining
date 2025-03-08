d6f2bb037d72f7b2369529da291f9d22b08caf6b
AnshuMishra9 <mishraanshu715@gmail.com>
2025-03-08 14:41:21 +0530

    2

--- file1/React1.js
+++ file1/React1.js
@@ -12,7 +12,7 @@ console.log(typeof(root));
 const parent = React.createElement("div",{id : "parent"},
     [   React.createElement("div",{id : "child1"},
         [React.createElement("h1",{},"i am an h1 tag"),
-        React.createElement("h2",{},"i am an h2 tag")]
+        Rea.createElement("h2",{},"i am an h2 tag")]
     ),
         React.createElement("div",{id : "child1"},
             [React.createElement("h1",{},"i am an h1 tag"),
